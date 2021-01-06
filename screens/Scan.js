28bd3382595fd69b383ecbcd8e2a83b82ac5b3e5
davisantosr <davi-rocha2@hotmail.com>
2021-01-06 20:59:12 -0300

    add screens| basic structure

---
+++ screens/Scan.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Scan = () => {
+  return (
+    <View>
+      <Text>
+        <Text>Scan</Text>
+      </Text>
+    </View>
+  )
+}
+
+export default Scan
