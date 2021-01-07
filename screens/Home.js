1ef3bb3d7d3d685449e89cd2aabfa4249e163028
davisantosr <davi-rocha2@hotmail.com>
2021-01-07 13:41:16 -0300

    add FlatList | special promo flatlist| renderPromos

--- screens/Home.js
+++ screens/Home.js
@@ -98,14 +98,71 @@ const Home = () => {
         description: "Don't miss it. Grab it now!"
     },
   ]
-  
+
   const [features, setFeatures] = React.useState(featuresData)
   const [specialPromos, setSpecialPromos] = React.useState(specialPromoData)
 
+  function renderPromos() {
+    const renderItem = ({item}) => (
+
+      <TouchableOpacity
+        style={{
+          marginVertical: SIZES.base, 
+          width: SIZES.width / 2.5, 
+        }}
+        onPress={() => console.log(item.title)}
+      >
+        <View
+          style={{
+            height: 80, 
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20, 
+            backgroundColor: COLORS.primary
+          }}
+        >
+          <Image 
+            source={images.promoBanner}
+            resizeMode='cover'
+            style={{
+              width: '100%', 
+              height: '100%',
+              borderTopLeftRadius: 20, 
+              borderTopRightRadius: 20, 
+            }}
+          />
+        </View>
+
+        <View
+          style={{
+            padding: SIZES.padding, 
+            backgroundColor: COLORS.lightGray, 
+            borderBottomLeftRadius: 20, 
+            borderBottomRightRadius: 20, 
+          }}
+        >
+          <Text style={{...FONTS.h4}}>{item.title}</Text>
+          <Text style={{...FONTS.body4}}>{item.description}</Text>
+        </View>
+
+      </TouchableOpacity>
+    )
+    return(
+      <FlatList 
+        contentContainerStyle={{paddingHorizontal: SIZES.padding *3}}
+        numColumns={2}
+        columnWrapperStyle={{justifyContent: 'space-between'}}
+        data={specialPromos}
+        keyExtractor={item => `${item.id}`}
+        renderItem={renderItem}
+        showsVerticalScrollIndicator={false}
+      />
+    )
+  }
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.white}}>
+      {renderPromos()}
+    </SafeAreaView>
   )
 }
 
