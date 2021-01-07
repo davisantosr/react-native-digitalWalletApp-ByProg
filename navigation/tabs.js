9aca6be26a3e14fc4ce256bcfe24545647a0e754
davisantosr <davi-rocha2@hotmail.com>
2021-01-07 11:25:12 -0300

    add RN svg | RN iphone-x-helper

---
+++ navigation/tabs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {
+  View, 
+  Image, 
+  TouchableOpacity, 
+  StyleSheet, 
+} from 'react-native'
+
+import { createBottomTabNavigator, BottomTabBar } from '@react-navigation/bottom-tabs'
+
+import { } from '../screens'
