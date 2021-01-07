57748b85cf472edd4fb929ca0cad9aef43a4ac27
davisantosr <davi-rocha2@hotmail.com>
2021-01-07 14:58:35 -0300

    add renderFeatures| renderFeatures header and renderItem | add renderPromoHeader

--- screens/Home.js
+++ screens/Home.js
@@ -169,15 +169,91 @@ const Home = () => {
     )
   }
 
+  function renderFeatures() {
+    const Header = () => {
+      return (
+        <View style={{marginBottom: SIZES.padding *2}}>
+          <Text style={{...FONTS.h3}}>Features</Text>
+        </View>
+      )
+    }
+
+    const renderItem = ({item}) => {
+      return (
+        <TouchableOpacity
+          style={{marginBottom: SIZES.padding*2, width: 60, alignItems: 'center'}}
+          onPress={() => console.log('description')}
+        >
+          <View
+            style={{
+              height: 50, 
+              width: 50, 
+              marginBottom: 5,
+              borderRadius: 20, 
+              backgroundColor: item.backgroundColor,
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}
+          >
+            <Image 
+              source={item.icon}
+              resizeMode='contain'
+              style={{
+                height: 20, 
+                width: 20, 
+                tintColor: item.color
+              }}
+            />
+            <Text style={{
+              textAlign: 'center', flexWrap: 'wrap', ...FONTS.body4}}>{item.description}</Text>
+
+          </View>
+        </TouchableOpacity>
+      )
+    }
+    return (
+      <FlatList 
+        ListHeaderComponent={Header}
+        data={features}
+        numColumns={4}
+        columnWrapperStyle={{justifyContent: 'space-between'}}
+        keyExtractor={item => `${item.id}`}
+        renderItem={renderItem}
+        style={{marginTop: SIZES.padding * 2}}
+      />
+    )
+  }
+
   function renderPromos() {
+
     const HeaderComponent = () => (
       <View>
         {renderHeader()}
         {renderBanner()}
+        {renderFeatures()}
+        {renderPromoHeader()}
       </View>
     )
-    const renderItem = ({item}) => (
 
+    const renderPromoHeader = () => (
+      <View
+        style={{
+          flexDirection: 'row',
+          marginBottom: SIZES.padding
+        }}
+      >
+        <View style={{flex:1}}>
+          <Text style={{...FONTS.h3}}>Special Promo</Text>
+        </View>       
+        <TouchableOpacity
+          onPress={() => console.log('View All')}
+        >
+          <Text style={{color: COLORS.gray, ...FONTS.body4}}>View All</Text>
+        </TouchableOpacity>
+      </View>
+    )
+    
+    const renderItem = ({item}) => (
       <TouchableOpacity
         style={{
           marginVertical: SIZES.base, 
