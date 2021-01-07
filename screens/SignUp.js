9a94f95ac15e4fd2caa1551bc49820498492ef0c
davisantosr <davi-rocha2@hotmail.com>
2021-01-06 22:04:08 -0300

    finished sign up header

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -17,6 +17,32 @@ import LinearGradient from 'react-native-linear-gradient'
 import { COLORS, SIZES, FONTS,  icons, images, } from '../constants'
 
 const SignUp = () => {
+
+  function renderHeader() {
+    return (
+      <TouchableOpacity
+        style={{
+          flexDirection: 'row', 
+          alignItems: 'center', 
+          marginTop: SIZES.padding * 6,
+          paddingHorizontal: SIZES.padding * 2, 
+        }}
+        onPress={() => console.log('Sign Up')}
+      >
+        <Image 
+          source={icons.back}
+          resizeMode={'contain'}
+          style={{
+            width: 20, 
+            height: 20, 
+            tintColor: COLORS.white
+          }}
+        />
+
+      </TouchableOpacity>
+    )
+  }
+
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : null}
@@ -26,6 +52,9 @@ const SignUp = () => {
         colors={[COLORS.lime, COLORS.emerald]}
         style={{flex: 1}}
       >
+        <ScrollView>
+          {renderHeader()}
+        </ScrollView>
 
       </LinearGradient>
     </KeyboardAvoidingView>
