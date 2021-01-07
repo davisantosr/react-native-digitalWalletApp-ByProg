501d4aeda651138eae1fc4b1c076837c21f4a0b4
davisantosr <davi-rocha2@hotmail.com>
2021-01-07 16:02:46 -0300

    add RNCamera| props

--- screens/Scan.js
+++ screens/Scan.js
@@ -1,12 +1,36 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { 
+  View, 
+  Text,
+  Image,
+  TouchableOpacity 
+} from 'react-native'
+
+import {RNCamera} from 'react-native-camera'
+import { COLORS, FONTS, SIZES, icons, images } from '../constants'
 
 const Scan = () => {
   return (
-    <View>
-      <Text>
-        <Text>Scan</Text>
-      </Text>
+    <View 
+      style={{flex: 1, backgroundColor: COLORS.transparent}}
+    >
+      <RNCamera
+        // ref={ref => {
+        //   this.camera = ref
+        // }}
+        style={{flex: 1}}
+        captureAudio={false}
+        type={RNCamera.Constants.Type.back}
+        flashMode={RNCamera.Constants.FlashMode.off}
+        androidCameraPermissionOptions={{
+          title: 'Permission to use camera',
+          message: 'Camera is required to code scanning',
+          buttonNegative: 'Cancel',
+          buttonPositive: 'Allow',
+        }}
+      >
+                  
+      </RNCamera>
     </View>
   )
 }
