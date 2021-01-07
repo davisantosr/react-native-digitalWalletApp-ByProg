8c848915b3b81debf88b925e3ada269911948e83
davisantosr <davi-rocha2@hotmail.com>
2021-01-07 16:14:22 -0300

    add RNCamera| Header | renderHeader

--- screens/Scan.js
+++ screens/Scan.js
@@ -9,7 +9,66 @@ import {
 import {RNCamera} from 'react-native-camera'
 import { COLORS, FONTS, SIZES, icons, images } from '../constants'
 
-const Scan = () => {
+const Scan = ({navigation}) => {
+
+  function renderHeader() {
+    return (
+      <View
+        style={{
+          flexDirection: 'row', 
+          marginTop: SIZES.padding * 4,
+          paddingHorizontal: SIZES.padding *3 
+        }}
+      >
+        <TouchableOpacity
+          style={{
+            width: 45,
+            alignItems: 'center', 
+            justifyContent: 'center', 
+          }}
+          onPress={() => navigation.navigate('Home') }
+        >
+          <Image 
+            source={icons.close}
+            style={{
+              height: 20, 
+              width: 20, 
+              tintColor: COLORS.white
+            }}
+          />          
+        </TouchableOpacity>
+
+        <View
+          style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text style={{color: COLORS.white, ...FONTS.body3}}>Scan for payment</Text>
+        </View>
+
+        <TouchableOpacity
+          style={{
+            height: 45, 
+            width: 45, 
+            backgroundColor: COLORS.green, 
+            borderRadius: 10, 
+            alignItems: 'center', 
+            justifyContent: 'center', 
+          }}
+          onPress={() => console.log('Info')}
+        >
+          <Image 
+            source={icons.info}
+            style={{
+              height: 25, 
+              width: 25, 
+              tintColor: COLORS.white
+            }}
+          />
+
+        </TouchableOpacity>
+
+      </View>
+    )
+  }
+
   return (
     <View 
       style={{flex: 1, backgroundColor: COLORS.transparent}}
@@ -29,7 +88,8 @@ const Scan = () => {
           buttonPositive: 'Allow',
         }}
       >
-                  
+        {renderHeader()}           
+               
       </RNCamera>
     </View>
   )
