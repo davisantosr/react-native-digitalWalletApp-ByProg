2855bf840c63ea2d90c21a9835098f80a6a60d4b
davisantosr <davi-rocha2@hotmail.com>
2021-01-07 14:31:53 -0300

    add header from HeaderComponent|renderHeader

--- screens/Home.js
+++ screens/Home.js
@@ -105,8 +105,43 @@ const Home = () => {
 
   function renderHeader() {
     return (
-      <View>
-        <Text>renderHeader</Text>
+      <View style={{flexDirection: 'row', marginVertical: SIZES.padding *2 }}>
+        <View style={{flex: 1}}>
+          <Text style={{...FONTS.h1}}>Hello</Text>
+          <Text style={{...FONTS.body2, color: COLORS.gray}}>Davi</Text>
+        </View>
+        <View style={{alignItems: 'center', justifyContent: 'center'}}>
+          <TouchableOpacity
+            style={{
+              height: 40, 
+              width: 40, 
+              justifyContent: 'center', 
+              alignItems: 'center',
+              backgroundColor: COLORS.lightGray
+            }}
+          >
+            <Image 
+              source={icons.bell}
+              style={{
+                width: 20,
+                height: 20, 
+                tintColor: COLORS.secondary
+              }}
+            />
+            <View style={{
+              position: 'absolute',
+              top: -5, 
+              right: -5, 
+              height: 10, 
+              width: 10, 
+              backgroundColor: COLORS.red, 
+              borderRadius: 5
+            }}>
+
+            </View>
+
+          </TouchableOpacity>
+        </View>
       </View>
     )
   }
